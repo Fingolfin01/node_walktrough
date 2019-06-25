e1132cf8c70f469701d3f47027ea92273e08c39b
Özcan <can.ceylan@me.com>
2019-06-25 13:10:45 +0200

    Finished ex3

--- formatting.js
+++ formatting.js
@@ -1,7 +1,12 @@
 
 const removeWhitespace = input => {
+    //trim space of the ends
+    input = 'thing with more words';
     let result = input.trim();
+
+    //collaps duplicate spaces
+    result = result.replace(/\s+/g, ' ');
+
     console.log(result);
 }
 
-removeWhitespace('    sao  tome');
